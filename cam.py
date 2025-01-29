95c8356817693022c5baef8d200e46a0afc96d38
kPereirathefirst <kaelyn.a.pereira@gmail.com>
2025-01-29 16:12:30 -0500

    detecting face + eyes through video

--- cam.py
+++ cam.py
@@ -1,27 +1,34 @@
 import cv2 as cv 
 
-#load image 
-img = cv.imread('image/stock1.jpg')
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cap = cv.VideoCapture(0)
 
 #load haarcascades 
 face_cascade = cv.CascadeClassifier('./haarcascades/haarcascade_frontalface_alt.xml')
 eye_cascade = cv.CascadeClassifier('./haarcascades/haarcascade_eye.xml')
 
-#detect faces and eyes 
-faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+#open camera 
+while True: 
+    ret, frame = cap.read() 
+    if not ret: 
+        break 
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-for (x1, y1, w1, h1) in faces: 
-    cv.rectangle(img, (x1, y1), (x1+w1, y1+h1), (218, 65, 103), 2)
-    roi_gray = gray[y1:y1+h1, x1:x1+w1]
-    roi_color = img[y1:y1+h1, x1:x1+w1]
+    #detect faces and eyes 
+    faces = face_cascade.detectMultiScale(gray, 1.3, 4)
 
-eyes = eye_cascade.detectMultiScale(roi_gray)
-for (x2, y2, w2, h2) in eyes: 
-    cv.rectangle(roi_color, (x2, y2), (x2+w2, y2+h2), (99, 163, 117), 2)
+    for (x1, y1, w1, h1) in faces: 
+        cv.rectangle(frame, (x1, y1), (x1+w1, y1+h1), (218, 65, 103), 2)
+        roi_gray = gray[y1:y1+h1, x1:x1+w1]
+        roi_color = frame[y1:y1+h1, x1:x1+w1]
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        for (x2, y2, w2, h2) in eyes: 
+            cv.rectangle(roi_color, (x2, y2), (x2+w2, y2+h2), (99, 163, 117), 2)
+
+    cv.imshow('frame', frame)
+
+    if cv.waitKey(1) & 0xFF == ord('q'): 
+        break
 
 #display image 
-cv.imshow('image', img)
-
-cv.waitKey(0)  
+cap.release()
 cv.destroyAllWindows()
