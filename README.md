82bd003cd78275ca60ae6d32b1c3b0be123fce5b
kPereirathefirst <kaelyn.a.pereira@gmail.com>
2025-02-03 10:39:30 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # filter
+
+It happens often that when I watch Pride and Prejudice (2005) and see Fitzwilliam Darcy on the big screen that I can't help but feel a sense of melancholy: "*I wish I looked like a rich man with no social skills*"
+
+Hold on....I can! 
+
+So I took to my computer and performed some basic face detection using Haar Cascades to overlay images of my favorite movie characters onto my face. 
+
+Here's how: 
+* cam.py contains a basic rundown of detecting a face and placing an image of Edward Cullen onto it 
+* filters.py generalizes this so I can overlay an image of the user's choice 
+* my flask app contains a homescreen with all my favorite movies + their respective characters, the user can select a character and it takes them to another page which opens up the camera on their device and places an image of the chosen character onto their face
+
+Next steps: 
+* create cleaner image overlays
+* more character choices
+* polished UI
+* hosting website on GitHub Pages
--- cam.py
+++ cam.py
@@ -5,7 +5,6 @@ cap = cv.VideoCapture(0)
 
 #load haarcascades 
 face_cascade = cv.CascadeClassifier('./haarcascades/haarcascade_frontalface_alt.xml')
-eye_cascade = cv.CascadeClassifier('./haarcascades/haarcascade_eye.xml')
 
 #load image 
 img = cv.imread('image/edward.jpg')
@@ -17,16 +16,8 @@ while True:
         break 
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
-    #detect faces and eyes 
+    #detect faces 
     faces = face_cascade.detectMultiScale(gray, 1.3, 4)
-
-    #creating reference points for face 
-    print(faces[0])
-    print(faces[0].split(" ")) #this doesn't work 
-    # x = faces[0][0]
-    # y = faces[0][1]
-    # w = faces[0][2]
-    # h = faces[0][3]
 
     for (x1, y1, w1, h1) in faces: 
         cv.rectangle(frame, (x1, y1), (x1+w1, y1+h1), (218, 65, 103), 2)
@@ -34,12 +25,6 @@ while True:
         frame[y1:y1+h1, x1:x1+w1] = img_resized
         roi_gray = gray[y1:y1+h1, x1:x1+w1]
         roi_color = frame[y1:y1+h1, x1:x1+w1]
-        """
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        if len(eyes) > 0:
-            for (x2, y2, w2, h2) in eyes: 
-                cv.rectangle(roi_color, (x2, y2), (x2+w2, y2+h2), (99, 163, 117), 2) """
-
     cv.imshow('frame', frame)
 
     if cv.waitKey(1) & 0xFF == ord('q'): 
