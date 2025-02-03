8dc3ceb80cf710a14ce992dd9b2f986005dff218
kPereirathefirst <kaelyn.a.pereira@gmail.com>
2025-02-02 21:55:21 -0500

    query works for edward

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,33 +1,17 @@
-from flask import Flask, render_template, Response 
+from flask import Flask, render_template, Response, request 
 import cv2 as cv 
 from filters import add_filter 
 
 app = Flask(__name__)
 
-def video_frame(): 
-    cam = cv.VideoCapture(0)
-    cam.set(cv.CAP_PROP_FRAME_WIDTH, 800)
-    cam.set(cv.CAP_PROP_FRAME_HEIGHT, 600)
-    while True:
-        ret, frame = cam.read()
-        if not ret: 
-            print("Camera failed")
-            break    
-        rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        _, buffer = cv.imencode('.jpg', frame)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n') 
-
 @app.route('/')
 def main_page(): 
     return render_template('index.html')
 
-@app.route('/video')
-def video(): 
-    return Response(video_frame(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
 @app.route('/process')
 def process(): 
+    character = request.args.get('char')
+    print(character)
     def video_frames():
         cam = cv.VideoCapture(0)
         cam.set(cv.CAP_PROP_FRAME_WIDTH, 800)
@@ -38,7 +22,7 @@ def process():
                 print("Camera failed")
                 break    
             rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-            frame = add_filter(frame, 'flask_app/filters/edward.jpg')
+            frame = add_filter(frame, character)
             _, buffer = cv.imencode('.jpg', frame)
             yield (b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
@@ -46,7 +30,10 @@ def process():
 
 @app.route('/cam')
 def cam(): 
-    return render_template('cam.html')
+    character = request.args.get('character')
+    print(character)
+    print("THIS IS THE FIRST ^^")
+    return render_template('cam.html', character = character)
 
 if __name__ == '__main__':
     app.run(debug=True)
